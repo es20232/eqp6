f8705b55c01909451c61844ca93783b337db97ce
Vinícius Alves <viniciusmra@gmail.com>
2024-01-17 23:43:06 -0300

    alteraçao nas rotas, amiaçoes e validaçao do formulário de login

--- projeto-final/frontend/tellemgram/src/index.js
+++ projeto-final/frontend/tellemgram/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // Router
-import{ createBrowserRouter, RouterProvider } from 'react-router-dom';
+import{ createBrowserRouter, RouterProvider, Route, Routes, BrowserRouter, useLocation   } from 'react-router-dom';
 
 import Home from "./routes/Home"
 import SignIn from "./routes/SignIn"
@@ -13,6 +13,9 @@ import AuthPages from './routes/AuthPages';
 import SignUp from './routes/SignUp';
 import Recover from './routes/Recover';
 import ErrorPage from './routes/ErrorPage';
+
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 
 
 const router = createBrowserRouter([
@@ -43,10 +46,35 @@ const router = createBrowserRouter([
 
 ])
 
+const RootComponent = () => {
+  const location = useLocation(); // Obter a localização atual
+
+  return (
+    <React.StrictMode>
+      <TransitionGroup>
+        <CSSTransition key={location.key} classNames="fade" timeout={300}>
+          <Routes>
+            <Route path="/auth/*" element={<AuthPages />} />
+            <Route path="/auth/entrar" element={<SignIn />} />
+            <Route path="/auth/cadastrar" element={<SignUp />} />
+            <Route path="/auth/recuperar" element={<Recover />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
+    </React.StrictMode>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth/*" element={<AuthPages />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
--- projeto-final/frontend/tellemgram/src/routes/AuthPages.js
+++ projeto-final/frontend/tellemgram/src/routes/AuthPages.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./AuthPages.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import SignIn from "./SignIn"
+import SignUp from './SignUp';
+import Recover from './Recover';
+
+import { AnimatePresence } from "framer-motion";
 
 const AuthPages = () => {
+    const location = useLocation();
     return(
         <div className="content">
             <div className="auth-form-background">
-                <Outlet />
+                <AnimatePresence mode='wait'>
+                    <Routes location={location} key={location.pathname}>
+                        <Route path="/" element={<Navigate to="entrar" />} />
+                        <Route key="teste1" path="entrar" element={<SignIn  key="teste1"/>} />
+                        <Route key="teste2" path="cadastrar" element={<SignUp key="teste2"/>} />
+                        <Route path="recuperar" element={<Recover />} />
+                    </Routes>
+                </AnimatePresence>
             </div>
             
         </div>
