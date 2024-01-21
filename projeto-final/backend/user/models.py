b8454a7236f69e7b98730ac07eaa334bbc9e54af
rfsousa <30508456+rfsousa@users.noreply.github.com>
2024-01-21 19:36:02 -0300

    Unique email and AbstractUser first and last name

--- projeto-final/backend/user/models.py
+++ projeto-final/backend/user/models.py
@@ -2,9 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
+    # name = models.CharField(max_length=50)
+    # surname = models.CharField(max_length=50)
     profile_image = models.BinaryField(null=True, blank=True)
+    email = models.EmailField(
+        unique=True,
+        blank=True
+    )
 
 class UserImage(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
